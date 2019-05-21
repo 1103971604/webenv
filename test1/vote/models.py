60436cc345c43c4e434efbbd2d3ec4e651c7ab37
赵宇杰 <1103971604@qq.com>
2019-05-21 21:25:01 +0800

    自己想法写的注册登录

--- test1/vote/models.py
+++ test1/vote/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+
 # Create your models here.
 class list(models.Model):
     title=models.CharField(max_length=50,verbose_name='标题')
